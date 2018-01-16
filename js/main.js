c058888c1b69a52c5c1aa891af62f2ae410cf886
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-01-16 15:09:02 -0800

    nav stuff

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// var myArray = ['<span>body type</span>', '<span>goals</span>', 'sport'];
+// var currentIndex = 0;
+// var selector = $('#selector');
+// var timer = setInterval(function(){
+// 		selector.animate({left: '-100%'}, 700, function () { 
+// 	    selector.hide();
+// 	    selector.html(myArray[currentIndex]);
+// 	    selector.css({left: '100%'});
+// 	    selector.show();
+// 	    selector.animate({left: '0'}, 700);
+// 	});
+// 	currentIndex++;
+// 	if (currentIndex == myArray.length) {
+// 		currentIndex = 0;
+// 	}
+//  }, 5600); //5600
+
