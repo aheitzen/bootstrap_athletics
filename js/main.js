c0b1ad9d4999897605b683b31f9fd540b6929969
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-01-21 10:55:56 -0800

    hero and others

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-var myArray = ['body type', 'goals', 'sport'];
-var currentIndex = 0;
-var selector = $('#selector');
+// var myArray = ['body type', 'goals', 'sport'];
+// var currentIndex = 0;
+// var selector = $('#selector');
+// var timer = setInterval(function(){
+// 		selector.animate({left: '-100%'}, 700, function () { 
+// 	    selector.hide();
+// 	    selector.html(myArray[currentIndex]);
+// 	    selector.css({left: '100%'});
+// 	    selector.show();
+// 	    selector.animate({left: '0'}, 700);
+// 	});
+// 	currentIndex++;
+// 	if (currentIndex == myArray.length) {
+// 		currentIndex = 0;
+// 	}
+//  }, 5600); //5600
+
+var myArray = ['body type', 'aesthetic', 'goals', 'sport', 'destination'];
+var currentIndex = 0
 var timer = setInterval(function(){
-		selector.animate({left: '-100%'}, 700, function () { 
-	    selector.hide();
-	    selector.html(myArray[currentIndex]);
-	    selector.css({left: '100%'});
-	    selector.show();
-	    selector.animate({left: '0'}, 700);
-	});
+	$("#selector").text(myArray[currentIndex]);
 	currentIndex++;
 	if (currentIndex == myArray.length) {
 		currentIndex = 0;
 	}
- }, 5600); //5600
+ }, 5600);
 
