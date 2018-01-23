514932ccf5f61a6db9dcb513a4483afd44fce248
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-01-23 13:51:49 -0800

    slider

--- js/main.js
+++ js/main.js
@@ -25,5 +25,22 @@
 // 	}
 //  }, 5600);
 
+var myArray = ['I love wearning my suites to the beach, pool, and on vacation!. <span> - Jane Doe</span><span><img class="testomonial-img rounded-circle" src="img/test-img-2.png" alt="testomonial img">','I can always find suites that suit my comfort level, modesty is a plus for me!<span><span>- Karen Carpenter</span><img class="testomonial-img rounded-circle" src="img/test-img-3.png" alt="testomonial img">' ];
+var currentIndex = 0;
+var selector = $('#selector');
+var timer = setInterval(function(){
+		selector.animate({left: '-100%'}, 700, function () { 
+	    selector.hide();
+	    selector.html(myArray[currentIndex]);
+	    selector.css({left: '100%'});
+	    selector.show();
+	    selector.animate({left: '0'}, 700);
+	});
+	currentIndex++;
+	if (currentIndex == myArray.length) {
+		currentIndex = 0;
+	}
+ }, 5600);
+
 new WOW().init();
 
