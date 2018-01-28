2c5d03e60f448c2e69b93a24d1484c40583652cd
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-01-28 15:12:19 -0800

    margin

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-// var myArray = ['body type', 'goals', 'sport'];
-// var currentIndex = 0;
-// var selector = $('#selector');
-// var timer = setInterval(function(){
-// 		selector.animate({left: '-100%'}, 700, function () { 
-// 	    selector.hide();
-// 	    selector.html(myArray[currentIndex]);
-// 	    selector.css({left: '100%'});
-// 	    selector.show();
-// 	    selector.animate({left: '0'}, 700);
-// 	});
-// 	currentIndex++;
-// 	if (currentIndex == myArray.length) {
-// 		currentIndex = 0;
-// 	}
-//  }, 5600); //5600
-
-// var myArray = ['body type', 'aesthetic', 'goals', 'sport', 'destination'];
-// var currentIndex = 0
-// var timer = setInterval(function(){
-// 	$("#selector").text(myArray[currentIndex]);
-// 	currentIndex++;
-// 	if (currentIndex == myArray.length) {
-// 		currentIndex = 0;
-// 	}
-//  }, 5600);
-
 var myArray = ['I love wearning my suites to the beach, pool, and on vacation!. <span> - Jane Doe</span><span><img class="testomonial-img rounded-circle" src="img/test-img-2.png" alt="testomonial img">','I can always find suites that suit my comfort level, modesty is a plus for me!<span><span>- Karen Carpenter</span><img class="testomonial-img rounded-circle" src="img/test-img-3.png" alt="testomonial img">' ];
 var currentIndex = 0;
 var selector = $('#selector');
